9043cc62569cb2f55c990e2e1decb485c5d90278
Mehuge <adf@sorcerer.co.uk>
2018-06-23 10:27:01 +0100

    Re-organise UI widgets used by Settings UI

---
+++ game/hud/src/widgets/UI/TabbedDialogWithSideMenu.tsx
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import { TabbedDialog, DialogButton, DialogTab } from './TabbedDialog';
+import { SideMenu, MenuOption, SideMenuContentInner } from './SideMenu';
+
+// Re-export these for convenience
+export { DialogButton, DialogTab, MenuOption, SideMenuContentInner };
+
+interface TabDefinition {
+  id: number | string;
+  tab: DialogButton;
+  options?: MenuOption[];
+  buttons?: DialogButton[];
+}
+
+export interface DialogDefinition {
+  name?: string;
+  title: string;
+  tabs: TabDefinition[];
+}
+
+interface TabbedDialogWithSideMenuProps {
+  definition: DialogDefinition;
+  onClose: () => void;
+  onAction?: (tab: DialogButton, action: DialogButton) => void;
+  children?: any;
+}
+
+/* tslint:disable:function-name */
+export function TabbedDialogWithSideMenu(props: TabbedDialogWithSideMenuProps) {
+  const { definition, children } = props;
+  const { tabs } = definition;
+  return (
+    <TabbedDialog
+      title={definition.title}
+	    tabs={definition.tabs.map(tab => tab.tab)}
+	    name={definition.name}
+      onClose={props.onClose}>{
+        (tab: DialogButton) => tabs.filter(tabDef => tabDef.tab === tab).map(tabDef =>
+          <DialogTab buttons={tabDef.buttons} onAction={button => props.onAction(tab, button)}>
+            <SideMenu name={definition.name} id={`${tabDef.id}`}
+              options={tabDef.options}>
+              {option => children && children(option, tab)}
+            </SideMenu>
+          </DialogTab>)
+    }</TabbedDialog>
+  );
+}
